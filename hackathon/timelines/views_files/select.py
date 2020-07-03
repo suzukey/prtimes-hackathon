8155a657afab0f329b64f0e0bd2c33f402ebc6e8
suzukey <suzukei0208@gmail.com>
2020-07-03 09:05:11 +0900

    コードスタイルの変更

--- hackathon/timelines/views_files/select.py
+++ hackathon/timelines/views_files/select.py
@@ -3,6 +3,7 @@
 import json
 import urllib.request
 
+
 def company(co_id):
 
     url = "https://hackathon.stg-prtimes.net/company_release/"
@@ -12,20 +13,23 @@ def company(co_id):
 
     i = 0
     page_num = 1
-    body = []
-    while(true):
+    body_temp = []
+    rel_id = []
+    while True:
         req = urllib.request.Request(
-            '{}{}{}?{}'.format(url, co_id, page_num, urllib.parse.urlencode(params))
+            '{}{}{}?{}'.format(url, co_id, page_num,
+                               urllib.parse.urlencode(params))
         )
-            with urllib.request.urlopen(req) as res:
-                body_temp = json.load(res)
+        with urllib.request.urlopen(req) as res:
+            body_temp = json.load(res)
         if body_temp["data"] == '':
             break
-        else :
-            for i in range(len(body_temp["data"]))
+        else:
+            for i in range(len(body_temp["data"])):
                 rel_id[i] = body_temp["data"][i]["release_id"]
 
     return rel_id
+
 
 def detail(co_id, cat_id):
 
@@ -39,26 +43,27 @@ def detail(co_id, cat_id):
     rel_id = company(co_id)
     while(rel_id != []):
         req = urllib.request.Request(
-            '{}{}{}?{}'.format(url, co_id, rel_id[i], urllib.parse.urlencode(params))
+            '{}{}{}?{}'.format(
+                url, co_id, rel_id[i], urllib.parse.urlencode(params))
         )
-            with urllib.request.urlopen(req) as res:
-                body_temp = json.load(res)
+        with urllib.request.urlopen(req) as res:
+            body_temp = json.load(res)
         if body_temp["data"] == '':
             break
-        elif body_temp["main_category_id"] == cat_id :
+        elif body_temp["main_category_id"] == cat_id:
             body[i] = {
-            #'page': 1,                             #page number
-            'data':[
-                {
-                    'company_id':   co_id,          #body_temp["company_id"]
-                    'release_id':   rel_id[i],      #body_temp["release_id"]
-                    'company_name':   body_temp["company_name"],
-                    'title':   body_temp["title"],
-                    'head':  body_temp["head"],     #リード文
-                    'main_image':   body_temp["main_image"],
-                    'created_at':   body_temp["created_at"],
-                }
-             ]
+                # 'page': 1,                             #page number
+                'data': [
+                    {
+                        'company_id': co_id,  # body_temp["company_id"]
+                        'release_id': rel_id[i],  # body_temp["release_id"]
+                        'company_name': body_temp["company_name"],
+                        'title': body_temp["title"],
+                        'head': body_temp["head"],  # リード文
+                        'main_image': body_temp["main_image"],
+                        'created_at': body_temp["created_at"],
+                    }
+                ]
             }
 
             i += 1
